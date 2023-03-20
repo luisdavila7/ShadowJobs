61472cacdde3b19dad662cafb4f04fb3c895e35b
Louis-Philippe <louis_philippe33575@hotmail.com>
2023-03-20 11:49:25 -0400

    Reworked the logic for a user to choose to register as a student or a restaurant and started building the student registration form

--- app/src/main/java/com/example/shadowjobs/StudentRegistration.java
+++ app/src/main/java/com/example/shadowjobs/StudentRegistration.java
@@ -1,15 +1,14 @@
 package com.example.shadowjobs;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import com.example.shadowjobs.R;
-
-public class Register extends AppCompatActivity {
+public class StudentRegistration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_student_registration);
     }
 }
