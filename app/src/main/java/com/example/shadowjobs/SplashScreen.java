e59518534de5d7c69fc0f7ab8243832cf91e819f
Behnam Ardalani <b.ardalani@gmail.com>
2023-04-06 08:24:39 -0400

    1 Comit

---
+++ app/src/main/java/com/example/shadowjobs/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.shadowjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.shadowjobs.ui.login.LoginActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
