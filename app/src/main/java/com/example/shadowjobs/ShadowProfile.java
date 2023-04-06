a56c01e851a83fdbc2266655df3414a0e2927e1d
Louis-Philippe <louis_philippe33575@hotmail.com>
2023-04-06 11:22:57 -0400

    Almost finished the Shadow Profile, added the description, the layout for the availability sub choices, and the bar chart with some mock data. Still need to add the menu at the top. I also did some minor changes to the login UI.

--- app/src/main/java/com/example/shadowjobs/ShadowProfile.java
+++ app/src/main/java/com/example/shadowjobs/ShadowProfile.java
@@ -2,13 +2,60 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
-public class ShadowProfile extends AppCompatActivity {/**/
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
+
+ public class ShadowProfile extends AppCompatActivity {/**/
+
+    BarChart barChart;
+
+    BarData barData;
+
+    BarDataSet barDataSet;
+
+    ArrayList barEntriesArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadow_profile);
+
+        barChart =  findViewById(R.id.BarChart);
+
+        getBarEntries();
+
+        barDataSet = new BarDataSet(barEntriesArrayList, "");
+
+        barData =  new BarData(barDataSet);
+
+        barChart.setData(barData);
+
+        barDataSet.setColors(Color.BLACK);
+
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
+    }
+
+    private void getBarEntries() {
+
+        barEntriesArrayList = new ArrayList<>();
+
+        barEntriesArrayList.add(new BarEntry(1f, 0));
+        barEntriesArrayList.add(new BarEntry(1f, 8));
+        barEntriesArrayList.add(new BarEntry(1f, 0));
+        barEntriesArrayList.add(new BarEntry(1f, 4));
+        barEntriesArrayList.add(new BarEntry(1f, 3));
+        barEntriesArrayList.add(new BarEntry(1f, 6));
+        barEntriesArrayList.add(new BarEntry(1f, 5));
+
     }
 }
