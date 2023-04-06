e19d5585ddc838a2713751c9c67af7b541a368ee
Louis-Philippe <louis_philippe33575@hotmail.com>
2023-04-06 12:27:31 -0400

    Changed the multiline text box's id in Shadow Profile, and create the Restaurants Profile UI from the same template as Shadow Profile

---
+++ app/src/main/java/com/example/shadowjobs/RestoProfile.java
@@ -0,0 +1,60 @@
+package com.example.shadowjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+public class RestoProfile extends AppCompatActivity {
+
+    BarChart barChart;
+
+    BarData barData;
+
+    BarDataSet barDataSet;
+
+    ArrayList barEntriesArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resto_profile);
+
+        barChart =  findViewById(R.id.RestoBarChart);
+
+        getBarEntries();
+
+        barDataSet = new BarDataSet(barEntriesArrayList, "");
+
+        barData =  new BarData(barDataSet);
+
+        barChart.setData(barData);
+
+        barDataSet.setColors(Color.BLACK);
+
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        barDataSet.setValueTextSize(16f);
+        barChart.getDescription().setEnabled(false);
+    }
+
+    private void getBarEntries() {
+
+        barEntriesArrayList = new ArrayList<>();
+
+        barEntriesArrayList.add(new BarEntry(1f, 0));
+        barEntriesArrayList.add(new BarEntry(1f, 8));
+        barEntriesArrayList.add(new BarEntry(1f, 0));
+        barEntriesArrayList.add(new BarEntry(1f, 4));
+        barEntriesArrayList.add(new BarEntry(1f, 3));
+        barEntriesArrayList.add(new BarEntry(1f, 6));
+        barEntriesArrayList.add(new BarEntry(1f, 5));
+    }
+}
