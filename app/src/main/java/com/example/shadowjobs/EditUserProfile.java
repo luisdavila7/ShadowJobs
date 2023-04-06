11aaa0ddd763dab5965d8a98da76a1c23bbb15f6
Luis Davila <luisdavila7@outlook.com>
2023-04-06 13:58:29 -0400

    Adding edit Restaurant, Shadow

---
+++ app/src/main/java/com/example/shadowjobs/EditUserProfile.java
@@ -0,0 +1,14 @@
+package com.example.shadowjobs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditUserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
--- app/src/main/java/com/example/shadowjobs/SplashScreen.java
+++ app/src/main/java/com/example/shadowjobs/SplashScreen.java
@@ -1,6 +1,7 @@
 package com.example.shadowjobs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
